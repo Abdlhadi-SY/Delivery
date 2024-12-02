c73703719bd44b1cfea4bf2e19c2e4389fba7f80
Abdlhadi <abdlhadesy@gmail.com>
2024-12-01 23:08:38 -0800

    creat

--- test/app/Http/Controllers/ProductController.php
+++ test/app/Http/Controllers/ProductController.php
@@ -11,9 +11,9 @@ class ProductController extends Controller
     // public function showProducts(Request $request){
     //     return Store::where('id',$request->id)->first()->products;
     // }
-    public function showProducts(){
-        return Product::all();
-    }
+    // public function showProducts(){
+    //     return Product::all();
+    // }
     public function showProduct(Request $request){
         return Product::where('name',$request->name)->first();
     }
